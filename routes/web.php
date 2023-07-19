39fac06ea39c2553836fbf0a11dc08422636d448
Taavi Toomsalu <queenshelob@mail.com>
2023-07-19 11:40:55 +0300

    Adding projects, home page with listings, single listing page, tags, search, post a job working, other things coming like register, login

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListingController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// All listings
+Route::get('/', [ListingController::class, 'index']);
+
+
+// Show create form
+Route::get('/listings/create', 
+[ListingController::class, 'create']);
+
+
+// Store listing data
+Route::post('/listings', 
+[ListingController::class, 'store']);
+
+
+// Single listing
+Route::get('/listings/{listing}', 
+[ListingController::class, 'show']);
